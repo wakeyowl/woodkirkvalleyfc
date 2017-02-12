db7c3cc61a9f25e29ae47ef7ec008c17f772ad3d
Iain Macdonald <iain.macdonald@williamhill.com>
2017-02-12 21:00:49 +0000

    initial commit

---
+++ member/views.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+
+from django.contrib.auth import logout, authenticate, login
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+
+def visitor_cookie_handler(request):
+    # Get the number of visits to the site.
+    # We use the COOKIES.get() function to obtain the visits cookie.
+    # If the cookie exists, the value returned is casted to an integer.
+    # If the cookie doesn't exist, then the default value of 1 is used.
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], "%Y-%m-%d %H:%M:%S")
+    # last_visit_time = datetime.now()
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).seconds > 0:
+        visits += 1
+        # update the last visit cookie now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        visits = 1
+        # set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
+    # update/set the visits cookie
+    request.session['visits'] = visits
+
+
+def index(request):
+    response = render(request, 'member/index.html', {})
+    return response
