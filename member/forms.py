61f1db067d2ce903126e6f260165ba38e09d5b4f
Iain Macdonald <iain.macdonald@williamhill.com>
2017-02-18 11:23:18 +0000

    Updated Registration form logic to make it flow easier for data integrity

--- member/forms.py
+++ member/forms.py
@@ -1,18 +1,35 @@
 from django import forms
 from django.contrib.auth.models import User
 
+from django.utils.safestring import mark_safe
+
 from member.models import UserMember
 
 
 class UserMemberForm(forms.ModelForm):
-    name = forms.CharField(max_length=128, required=True, help_text="Please enter your full name.")
-    address = forms.CharField(max_length=128, required=False)
-    postcode = forms.CharField(max_length=12, required=False)
-    email = forms.EmailField(required=True)
+    name = forms.CharField(max_length=128, required=True, widget=forms.TextInput(attrs={'placeholder': 'John Smith'}))
+    address_street = forms.CharField(max_length=128, required=True, widget=forms.TextInput(attrs={'placeholder': '1 '
+                                                                                                                  'Smith Street'}))
+    address_town_city = forms.CharField(max_length=128, required=True,
+                                        widget=forms.TextInput(attrs={'placeholder': 'Leeds '}))
+    postcode = forms.CharField(max_length=12, required=True, widget=forms.TextInput(attrs={'placeholder': 'WF3 1UA'}))
     phone = forms.IntegerField(required=True)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
-    consent = forms.BooleanField()
+    consent = forms.BooleanField(help_text=mark_safe("By consenting you are agreeing to the following: "
+                                                     "<ul> "
+                                                     "<li>I agree to my child receiving medication as "
+                                                     "instructed on their record </li>"
+                                                     "<li>I agree to emergency treatment by a qualified first "
+                                                     "aider representing the club should the need arise </li>"
+                                                     "<li>I agree to my child being photographed for the sole "
+                                                     "usage in the promotion or celebration of activities of the "
+                                                     "club </li>"
+                                                     "<li>I agree to emergency treatment including the "
+                                                     "administration of anaesthetics or blood transfusions as "
+                                                     "considered necessary by the medial professionals present "
+                                                     "should the need arise </li>"
+                                                     "</ul>"))
 
     class Meta:
         model = UserMember
-        fields = ('name', 'address', 'postcode', 'email', 'phone', 'consent' )
+        fields = ('name', 'address_street', 'address_town_city', 'postcode', 'phone', 'consent')
--- member/models.py
+++ member/models.py
@@ -6,9 +6,9 @@ from django.template.defaultfilters import slugify
 class UserMember(models.Model):
     user = models.OneToOneField(User)
     name = models.CharField(max_length=128, unique=True)
-    address = models.CharField(max_length=128)
+    address_street = models.CharField(max_length=128)
+    address_town_city = models.CharField(max_length=128)
     postcode = models.CharField(max_length=12)
-    email = models.EmailField()
     phone = models.IntegerField()
     slug = models.SlugField(unique=True)
     consent = models.BooleanField()
@@ -23,10 +23,13 @@ class UserMember(models.Model):
 
 class Player(models.Model):
     name = models.CharField(max_length=128)
-    birthdate = models.DateField()
+    birth_date = models.DateField()
     sex = models.CharField(max_length=1)
     member_parent = models.ForeignKey(UserMember)
     medical_details = models.TextField()
+
+    class Meta:
+        unique_together = ('birth_date', 'name',)
 
     def __str__(self):
         return self.name
