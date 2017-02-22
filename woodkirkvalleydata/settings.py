5cc7a2210dbcf419a1693335cd80dd904580a91c
Iain Macdonald <iain.macdonald@williamhill.com>
2017-02-22 21:23:27 +0000

    Added a DATE_INPUT_FORMAT

--- woodkirkvalleydata/settings.py
+++ woodkirkvalleydata/settings.py
@@ -123,6 +123,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+DATE_INPUT_FORMATS = ('%dd-%mm-%YYYY')
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
