e413aef7145de70ba3d9af31919e2679aef9e50d
Iain Macdonald <iain.macdonald@williamhill.com>
2017-02-23 21:19:09 +0000

    Updated to connect to production db

--- woodkirkvalleydata/settings.py
+++ woodkirkvalleydata/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         # The following settings are not used with sqlite3:
-        'NAME': 'woodkirkdata',
+        'NAME': 'wakeyowl$woodkirkdata',
         'USER': 'wakeyowl',
         'PASSWORD': 'v0n-neumann',
         'HOST': 'wakeyowl.mysql.pythonanywhere-services.com',  # Empty for localhost through domain sockets or   '127.0.0.1' for localhost through TCP.
