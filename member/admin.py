fc9d1e172a5cb137ec50eb8f02442443d053ec6e
Iain Macdonald <iain.macdonald@williamhill.com>
2017-05-10 14:48:30 +0100

    Updated the admin forms

--- member/admin.py
+++ member/admin.py
@@ -11,8 +11,16 @@ class TeamManagerAdmin(admin.ModelAdmin):
     list_display = ('full_name', 'team')
 
 
-admin.site.register(Payments)
-admin.site.register(Player)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('player_id', 'payment_amount', 'date_taken', 'paymentType', )
+    list_filter = ('player_id', 'paymentType',)
+
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'gender', 'birthdate', 'medical_details', 'member_parent_id', 'is_active', 'manager_id')
+    list_filter = ('manager_id', 'is_active',)
+
+admin.site.register(Payments, PaymentAdmin)
+admin.site.register(Player, PlayerAdmin)
 admin.site.register(UserMember, UserMemberAdmin)
 admin.site.register(TeamManagers, TeamManagerAdmin)
 
