defe4fb0bd98a1de3aade9f72709db9430e340bb
Iain Macdonald <iain.macdonald@williamhill.com>
2017-05-13 08:25:59 +0100

    updated new allowed hosts

--- woodkirkvalleydata/settings.py
+++ woodkirkvalleydata/settings.py
@@ -147,7 +147,7 @@ DATE_INPUT_FORMATS = ('%dd-%mm-%YYYY')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = "/home/wakeyowl/woodkirkvalleydata/static"
+STATIC_ROOT = "/home/wvfc/woodkirkvalleydata/static"
 STATIC_URL = '/static/'
 
 REGISTRATION_OPEN = True
