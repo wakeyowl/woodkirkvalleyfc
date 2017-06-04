334061ce99dd6154e49807917ba2d25df6bbc398
Iain Macdonald <iain.macdonald@williamhill.com>
2017-06-04 17:33:54 +0100

    Updated Settings for Correct Date format

--- woodkirkvalleydata/settings.py
+++ woodkirkvalleydata/settings.py
@@ -146,11 +146,12 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False
 
 USE_TZ = True
 
 DATE_INPUT_FORMATS = ('%dd-%mm-%YYYY')
+DATE_FORMAT = 'j N, Y'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
